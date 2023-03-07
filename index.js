ffefbbe976d3cf1a91a6a2e2d20bed42a6b6c1b0
Jairo Castro Flores <ochante.995@gmail.com>
2023-03-07 17:46:04 -0300

    task2 java

---
+++ index.js
@@ -0,0 +1,54 @@
+
+let eventos = data["events"];
+let eventos_recientes = [];
+let eventos_pasados = [];
+
+
+// eventos.forEach(event => {
+//     console.log(`nombre del evento : ${event["name"]}`);
+// });
+
+function crear_cartas(eventos, id_contenedor) {
+  let body = ``;
+  let update_cart = document.getElementById(id_contenedor)
+  eventos.forEach(element => {
+    body += `
+        <div class="card cartas" style="width: 18rem;">
+        <img src=${element["image"]} class="carta " alt="jugador1">
+        <div class="card-body">
+          <h5 class="card-title">${element["name"]}</h5>
+          <p class="card-text">${element["description"]}</p>
+
+        </div>
+        <div class="boton_precio card-body">
+          <span>$ ${element["price"]} </span>
+          <a href="#" class="btn btn-primary ">Ver mas</a>
+        </div>
+      </div>
+      `;
+    update_cart.innerHTML = body;
+  });
+}
+
+function filtrar_eventos(event) {
+  event.forEach(element => {
+    if (element["date"] < "2021-12-12") {
+      eventos_pasados.push(element);
+    }
+    else if (element["date"] >= "2021-12-12") {
+      eventos_recientes.push(element);
+    }
+  });
+}
+
+ crear_cartas(eventos, "nueva_carta");
+
+// let primer_checkbox = document.getElementById("filtro1");
+// // console.log(primer_checkbox[0]["checked"]);
+// console.log(primer_checkbox["checked"]);
+// // "category":"Food Fair",
+// if (primer_checkbox["checked"]== true){
+//        crear_cartas(eventos, "nueva_carta");
+//     }
+
+
---
+++ past_events.js
@@ -0,0 +1,47 @@
+
+let eventos = data["events"];
+let eventos_recientes = [];
+let eventos_pasados = [];
+
+
+// eventos.forEach(event => {
+//     console.log(`nombre del evento : ${event["name"]}`);
+// });
+
+function crear_cartas(eventos, id_contenedor) {
+  let body = ``;
+  let update_cart = document.getElementById(id_contenedor)
+  eventos.forEach(element => {
+    body += `
+        <div class="card cartas" style="width: 18rem;">
+        <img src=${element["image"]} class="carta " alt="jugador1">
+        <div class="card-body">
+          <h5 class="card-title">${element["name"]}</h5>
+          <p class="card-text">${element["description"]}</p>
+
+        </div>
+        <div class="boton_precio card-body">
+          <span>$ ${element["price"]} </span>
+          <a href="#" class="btn btn-primary ">Ver mas</a>
+        </div>
+      </div>
+      `;
+    update_cart.innerHTML = body;
+  });
+}
+
+function filtrar_eventos(event) {
+  event.forEach(element => {
+    if (element["date"] < "2021-12-12") {
+      eventos_pasados.push(element);
+    }
+    else if (element["date"] >= "2021-12-12") {
+      eventos_recientes.push(element);
+    }
+  });
+}
+
+
+filtrar_eventos(eventos);
+
+crear_cartas(eventos_pasados,"eventos_pasados");
